a536260abd722abefc059630c6967f7e9357d1bf
RitikaJain8818 <ritikajain49026@gmail.com>
2023-01-02 23:48:21 +0530

    igniting the app with parcel

--- Code/App.js
+++ Code/App.js
@@ -1,7 +1,53 @@
-const heading = document.createElement("h1");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-heading.innerHTML = "Hello World?";
 
-const root = document.getElementById("root");
+/*
+Created a server
+HOT Module Replacement
+File Watcher Algorithm : Written in C++
+Caching during Development
+Bundling
+Minify
+Cleaning our Code
+Dev and Production Build
+Super fast build algorithm
+Image Optimization
+Compression
+Compatible with older versions of Browser
+HTTPS on dev -> npx parcel index.html --https
+PORT Number
+Consistent Hashing Algorithm to To cache things up
+Zero Configuration
 
-root.appendChild(heading);
+*/
+
+const heading1 = React.createElement(
+  'h1',
+  {
+    id: 'title',
+  },
+  'Bonjour!!',
+)
+
+const heading2 = React.createElement(
+  'h2',
+  {
+    id: 'title',
+  },
+  "What's up?",
+)
+
+console.log(heading1)
+
+const container = React.createElement(
+  'div',
+  {
+    id: 'container',
+  },
+  [heading1, heading2],
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(container);
