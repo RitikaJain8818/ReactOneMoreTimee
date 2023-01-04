1c6c7b11efc8cbe5da626c2cfea7b0246c8b8646
RitikaJain8818 <ritikajain49026@gmail.com>
2023-01-05 01:17:52 +0530

    Laying the Foundation

--- Code/App.js
+++ Code/App.js
@@ -1,6 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-
 
 /*
 Created a server
@@ -19,35 +17,56 @@ HTTPS on dev -> npx parcel index.html --https
 PORT Number
 Consistent Hashing Algorithm to To cache things up
 Zero Configuration
-
+Tree Shaking: Removing Unwanted Code
+Transitive Dependencies
 */
 
-const heading1 = React.createElement(
-  'h1',
-  {
-    id: 'title',
-  },
-  'Bonjour!!',
-)
+//JSX => React.createElement => Object => HTML(DOM)
+/*
+data = api.getData()
 
-const heading2 = React.createElement(
-  'h2',
-  {
-    id: 'title',
-  },
-  "What's up?",
-)
+Let's say api got hacked 😮
+Hackers can insert any malicious piece of JS code. 
+XSS Attack: Cross-site scripting attack.
+But But, JSX does the sanitization. So, it keeps the app safe.
+*/
+const heading = (
+  <h1 id="title" key="h1">
+    Bonjour!!
+  </h1>
+);
 
-console.log(heading1)
+const MostestFavouritestSong = () => {
+  return (
+    <p>All Too Well - 10 Minutes Version</p>
+  )
+};
 
-const container = React.createElement(
-  'div',
-  {
-    id: 'container',
-  },
-  [heading1, heading2],
-)
+/*
+React Components
+Functional Component : NEW
+Class Based Component : OLD
+*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const dieHardFan = "I'm Taylor's Version Swiftie";
 
-root.render(container);
+
+/* 
+Multiple children in parent element? Add keys to children to help React to uniquely identify the child element.
+*/
+const HeaderComponent = () => {
+  return (
+    <div id="container">
+      {heading}
+      <h2 key="h2">What's up</h2>
+      <h3 key="h3">How is it going?</h3>
+      {dieHardFan}
+      <MostestFavouritestSong /> {/*Component Composition*/}
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root.
+root.render(<HeaderComponent />);
